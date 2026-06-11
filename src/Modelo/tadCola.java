2f9d1b3b8d18ecb853e10efea400ab614f62be34
zDefcon21 <antholozadarequelme@gmail.com>
2026-06-10 19:26:22 -0500

    Formatea el código y agrega función de búsqueda en la cola Se mejora la legibilidad del código al aplicar un formato consistente. Además, se implementa la función 'buscar' para permitir la búsqueda de elementos en la cola.

--- src/Modelo/tadCola.java
+++ src/Modelo/tadCola.java
@@ -1,121 +1,147 @@
 package Modelo;
 
 public class TadCola<T> implements Cola<T> {
-	private NodoCola<T> principio;
-	private NodoCola<T> fin;
-	private String nombre;
+    private NodoCola<T> principio;
+    private NodoCola<T> fin;
+    private String nombre;
 
-	public TadCola() {
-		principio = null;
-		fin = null;
-	}
+    public TadCola() {
+        principio = null;
+        fin = null;
+    }
 
-	public TadCola(String nombre) {
-		super();
-		principio = null;
-		fin = null;
-		this.nombre = nombre;
-	}
+    public TadCola(String nombre) {
+        super();
+        principio = null;
+        fin = null;
+        this.nombre = nombre;
+    }
 
-	public String getNombre() {
-		return nombre;
-	}
+    public String getNombre() {
+        return nombre;
+    }
 
-	public boolean colaVacia() {
-		return principio == null;
-	}
+    public boolean colaVacia() {
+        return principio == null;
+    }
 
-	public void eliminarCola() {
-		principio = null;
-		fin = null;
-	}
+    public void eliminarCola() {
+        principio = null;
+        fin = null;
+    }
 
-	public T primero() throws ColaVacia {
-		if (colaVacia())
-			throw new ColaVacia("La cola está vacía");
-		else
-			return principio.dato;
-	}
+    public T primero() throws ColaVacia {
+        if (colaVacia())
+            throw new ColaVacia("La cola está vacía");
+        else
+            return principio.dato;
+    }
 
-	public void encolar(T x) {
-		NodoCola<T> aux;
-		aux = new NodoCola<>(x, null);
-		if (principio == null) {
-			principio = aux;
-			fin = aux;
-		} else {
-			fin.siguiente = aux;
-			fin = aux;
-		}
-	}
+    public void encolar(T x) {
+        NodoCola<T> aux;
+        aux = new NodoCola<>(x, null);
+        if (principio == null) {
+            principio = aux;
+            fin = aux;
+        } else {
+            fin.siguiente = aux;
+            fin = aux;
+        }
+    }
 
-	public T desencolar() throws ColaVacia {
-		if (colaVacia()) {
-			throw new ColaVacia("Desencolar: La cola está vacía");
-		} else {
-			T resultado;
-			resultado = principio.dato;
-			principio = principio.siguiente;
-			if (principio == null)
-				fin = null;
-			return resultado;
-		}
-	}
+    public T desencolar() throws ColaVacia {
+        if (colaVacia()) {
+            throw new ColaVacia("Desencolar: La cola está vacía");
+        } else {
+            T resultado;
+            resultado = principio.dato;
+            principio = principio.siguiente;
+            if (principio == null)
+                fin = null;
+            return resultado;
+        }
+    }
 
-	public void quitarPrimero() throws ColaVacia {
-		if (colaVacia()) {
-			throw new ColaVacia("Quitar primero: La cola est� vac�a");
-		} else {
-			principio = principio.siguiente;
-			if (principio == null)
-				fin = null;
-		}
-	}
+    public void quitarPrimero() throws ColaVacia {
+        if (colaVacia()) {
+            throw new ColaVacia("Quitar primero: La cola est� vac�a");
+        } else {
+            principio = principio.siguiente;
+            if (principio == null)
+                fin = null;
+        }
+    }
 
-	public void mostrarEstadoCola() {
-		System.out.println("Estado de la cola:");
-		System.out.println("Número de elementos: " + this.numElemCola());
-		if (!colaVacia()) {
-			System.out.println("Primer elemento - Clave: " + principio.dato);
-			System.out.println("Último elemento - Clave: " + fin.dato);
-		}
-	}
+    public void mostrarEstadoCola() {
+        System.out.println("Estado de la cola:");
+        System.out.println("Número de elementos: " + this.numElemCola());
+        if (!colaVacia()) {
+            System.out.println("Primer elemento - Clave: " + principio.dato);
+            System.out.println("Último elemento - Clave: " + fin.dato);
+        }
+    }
 
-	public void imprimirCola() {
-		NodoCola<T> aux;
-		System.out.print("Estado de la cola " + this.getNombre() + ": ");
-		aux = principio;
-		while (aux != null) {
-			System.out.print(aux.dato + " ");
-			aux = aux.siguiente;
-		}
-		System.out.println();
-	}
+    public void imprimirCola() {
+        NodoCola<T> aux;
+        System.out.print("Estado de la cola " + this.getNombre() + ": ");
+        aux = principio;
+        while (aux != null) {
+            System.out.print(aux.dato + " ");
+            aux = aux.siguiente;
+        }
+        System.out.println();
+    }
 
-	public int numElemCola() {
-		NodoCola<T> aux;
-		int resul;
+    public int numElemCola() {
+        NodoCola<T> aux;
+        int resul;
 
-		aux = principio;
-		resul = 0;
-		while (aux != null) {
-			++resul;
-			aux = aux.siguiente;
-		}
-		return resul;
-	}
+        aux = principio;
+        resul = 0;
+        while (aux != null) {
+            ++resul;
+            aux = aux.siguiente;
+        }
+        return resul;
+    }
 
-	public void invertirCola() {
-		T elem;
-		if (!colaVacia()) {
-			try {
-				elem = desencolar();
-				invertirCola();
-				encolar(elem);
-			} catch (ColaVacia e) {
-				
+    public void invertirCola() {
+        T elem;
+        if (!colaVacia()) {
+            try {
+                elem = desencolar();
+                invertirCola();
+                encolar(elem);
+            } catch (ColaVacia e) {
+
+            }
+
+        }
+    }
+
+    public boolean buscar(T elemento) {
+        boolean encontrado = false;
+		boolean on = false;
+        // NodoCola<T> aux = principio;
+        if (principio != null) {
+            encontrado = buscarR(principio, elemento, on);
+        }
+
+        return encontrado;
+
+    }
+
+    private boolean buscarR(NodoCola aux, T elemento, boolean on) {
+        if (aux != null && on == false) {
+            if (aux.dato.equals(elemento)) {
+                on = true;
+            } else {
+				aux = aux.siguiente;
+				on = buscarR(aux, elemento, on);
 			}
-			
-		}
-	}
+        }
+
+		return on;
+    }
+
 }
