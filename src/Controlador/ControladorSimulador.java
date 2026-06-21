3e065c4618a6d910faf18617de4d9ef540d655c6
zDefcon21 <antholozadarequelme@gmail.com>
2026-06-21 03:46:53 -0500

    Agrega controlador para la simulación de cola
    Implementa la clase ControladorSimulador que gestiona la interacción entre la vista FrmSimulador y el modelo TadCola. Se añade un ActionListener para el botón de encolar, permitiendo que los datos ingresados en la interfaz se encolen en el modelo y se actualice la vista correspondiente.

--- src/Controlador/ControladorSimulador.java
+++ src/Controlador/ControladorSimulador.java
@@ -1,4 +1,39 @@
 package Controlador;
 
+import Modelo.TadCola;
+import Vista.FrmSimulador;
+import Vista.PanelMemoria;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class ControladorSimulador {
+
+    private TadCola<String> modeloCola;
+    private FrmSimulador vistaPrincipal;
+
+    public ControladorSimulador(TadCola<String> modeloCola, FrmSimulador vistaPrincipal){
+        this.modeloCola = modeloCola;
+        this.vistaPrincipal = vistaPrincipal;
+
+        // detecta el boton encolar de la interfaz
+        this.vistaPrincipal.getBtnEncolar().addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                ejecutarEncolar();
+            }
+        });
+    }
+
+    private void ejecutarEncolar(){
+
+        String dato = vistaPrincipal.getTxtValorEncolar().getText();
+        modeloCola.encolar(dato);
+        vistaPrincipal.getPanelMemoria().setCola(modeloCola);
+        vistaPrincipal.getPanelMemoria().repaint();
+        vistaPrincipal.txtValor("");
+
+    }
+
+
 }
