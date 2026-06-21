6503b20948578e18125677ea68c0af05f8f03ad1
zDefcon21 <antholozadarequelme@gmail.com>
2026-06-21 03:47:17 -0500

    Agrega inicialización del modelo y controlador en Main
    Se configura el modelo de la cola y se inicializa el controlador
    para la simulación en la clase Main, mejorando la estructura
    del simulador de cola.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,7 @@
 package main;
 
+import Controlador.ControladorSimulador;
+import Modelo.TadCola;
 import Vista.FrmSimulador;
 import com.formdev.flatlaf.FlatLightLaf;
 
@@ -8,7 +10,10 @@ import com.formdev.flatlaf.FlatLightLaf;
 public class Main {
     public static void main(String[] args) {
         FlatLightLaf.setup();
+        TadCola<String> modelo = new TadCola<>("Cola Principal");
         FrmSimulador window = new FrmSimulador();
+
+        ControladorSimulador controlador = new ControladorSimulador(modelo, window);
         window.setVisible(true);
         window.setResizable(false);
         window.setLocationRelativeTo(null);
