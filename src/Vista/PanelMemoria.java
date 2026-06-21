b5b2820974166d2a03b1b2bb1b8a9d4b9b5a4ff1
zDefcon21 <antholozadarequelme@gmail.com>
2026-06-21 03:47:25 -0500

    Agrega funcionalidad de dibujo en PanelMemoria
    Implementa métodos para dibujar referencias y nodos en el panel de memoria, permitiendo visualizar el estado de la cola. Se añade lógica para manejar colas vacías y se asegura la correcta representación gráfica de los elementos en la cola.

--- src/Vista/PanelMemoria.java
+++ src/Vista/PanelMemoria.java
@@ -1,4 +1,95 @@
 package Vista;
 
-public class PanelMemoria {
+import Modelo.Cola;
+import Modelo.ColaVacia;
+import Modelo.TadCola;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class PanelMemoria extends JPanel {
+    private Cola<String> colaDibujar;
+
+    public void setCola(Cola<String> colaDibujar) {
+        this.colaDibujar = colaDibujar;
+    }
+
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g); // limpiamos la pizarra de dibujo primero
+
+        if (colaDibujar == null) { // si la cola aun no esta creada
+            return;
+        }
+
+        if (colaDibujar.colaVacia()) {
+            dibujarReferencias(g);
+        } else {
+            dibujarNodos(g);
+        }
+
+
+
+        /*g.drawRect(x, y, ancho, alto); // dibujar rectangulo
+        g.drawString("Dato Test", x + 15, y  + 25);*/
+    }
+
+    private void dibujarReferencias(Graphics g) {
+        int xPunteros = 100;
+        int lado = 50;
+
+        // Puntero Fin
+        int yFin = 80;
+        g.drawString("Fin", xPunteros + 15, yFin - 10);
+        g.drawRoundRect(xPunteros, yFin, lado, lado, 15, 15);
+        g.drawLine(xPunteros, yFin + lado, xPunteros + lado, yFin); // linea diagonal
+
+        // Puntero Principio
+        int yPrincipio = 200;
+        g.drawRoundRect(xPunteros, yPrincipio, lado, lado, 15, 15);
+        g.drawLine(xPunteros, yPrincipio + lado, xPunteros + lado, yPrincipio); // linea diagonal
+        g.drawString("Principio", xPunteros + 5, yPrincipio + lado + 20);
+    }
+
+    private void dibujarNodos(Graphics g) {
+        int x = 50;
+        int y = 350;
+        int ancho = 60;
+        int alto = 40;
+        int espacio = 40;
+
+        Cola<String> colaAux = new TadCola<>("Aux");
+        int i = 0;
+        int totalElementos = colaDibujar.numElemCola();
+
+        try {
+            while (!colaDibujar.colaVacia()) {
+                String dato = colaDibujar.desencolar();
+
+                int xi = (x + i * (ancho + espacio)); // calcular la posición x para el rectángulo actual
+
+                g.drawRect(xi, y, ancho, alto);
+                g.drawString(dato, xi + 25, y + 25);
+
+                if (i < totalElementos - 1) {
+                    int xSalida = xi + ancho;
+                    int xLlegada = xSalida + espacio;
+                    int yCentro = y + (alto / 2);
+
+                    g.drawLine(xSalida, yCentro, xLlegada, yCentro); // flecha que conecta al nodo siguiente
+                }
+
+                colaAux.encolar(dato); // mandamos el dato a la cola de respaldo
+                i++; // avanzamos al siguiente nodo
+            }
+
+            while (!colaAux.colaVacia()) {
+                colaDibujar.encolar(colaAux.desencolar()); // encolamos datos a la colaOriginal
+            }
+        } catch (ColaVacia e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
 }
