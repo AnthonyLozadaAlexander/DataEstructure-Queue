4787f61b0f16ba55f516f5db8cbfa0441e52ff31
zDefcon21 <antholozadarequelme@gmail.com>
2026-06-21 22:21:10 -0500

    Ajusta la indentación y el formato en Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,19 +1,22 @@
 package main;
 
 import Controlador.ControladorSimulador;
+import Modelo.ColaVacia;
 import Modelo.TadCola;
 import Vista.FrmSimulador;
 import com.formdev.flatlaf.FlatLightLaf;
 
 public class Main {
     public static void main(String[] args) {
+
         FlatLightLaf.setup();
         TadCola<String> modelo = new TadCola<>("Cola Principal");
         FrmSimulador window = new FrmSimulador();
 
-        ControladorSimulador controlador = new ControladorSimulador(modelo, window);
-        window.setVisible(true);
-        window.setResizable(false);
-        window.setLocationRelativeTo(null);
+            ControladorSimulador controlador = new ControladorSimulador(modelo, window);
+            window.setVisible(true);
+            window.setResizable(false);
+            window.setLocationRelativeTo(null);
+
     }
 }
