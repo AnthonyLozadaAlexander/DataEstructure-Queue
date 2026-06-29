235b6f1b776d5a29ecdc86c41bf68fc73903bdfc
zDefcon21 <antholozadarequelme@gmail.com>
2026-06-29 03:12:40 -0500

    Se arreglo el formato del main

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,15 +8,13 @@ import com.formdev.flatlaf.FlatIntelliJLaf;
 
 public class Main {
     public static void main(String[] args) {
-
         FlatIntelliJLaf.setup();
         TadCola<String> modelo = new TadCola<>("Cola Principal");
         FrmSimulador vista = new FrmSimulador();
-
-            ControladorSimulador controlador = new ControladorSimulador(modelo, vista);
-            vista.setVisible(true);
-            vista.setResizable(false);
-            vista.setLocationRelativeTo(null);
+        ControladorSimulador controlador = new ControladorSimulador(modelo, vista);
+        vista.setVisible(true);
+        vista.setResizable(false);
+        vista.setLocationRelativeTo(null);
 
     }
 }
